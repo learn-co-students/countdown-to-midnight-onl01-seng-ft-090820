7dd948409f25ceca86f3681833cdf938301b7fe5
Leah Schlackman <leahemily1@gmail.com>
2020-09-09 20:31:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -8,10 +8,10 @@ end
   
 
 def countdown_with_sleep(n)
-  sleep(5)
   while n > 0 
     puts "#{n} SECOND(S)!"
-    n -= 1 
+    n -= 1
+    sleep(1)
   end
-  "HAPPY NEW YEAR!"
+    "HAPPY NEW YEAR!"
 end
