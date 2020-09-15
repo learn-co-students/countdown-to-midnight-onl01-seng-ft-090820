5c07c8c201b39c6c001fbfd66f0ed4a63645e4c1
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-15 08:40:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,12 @@ while number < 10
   num -= 1
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+while num > 0
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+end
+  "HAPPY NEW YEAR!"
+end
