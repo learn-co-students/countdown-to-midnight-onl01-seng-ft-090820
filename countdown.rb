ca2f67f11f141c81f607296390207d28624baaa3
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-15 08:46:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -14,3 +14,11 @@ while number > 0
 end
   "HAPPY NEW YEAR!"
 end
+
+ def countdown_with_sleep(count)
+    while count > 0
+      count-=1
+      sleep(1)
+    end
+      puts "0 left. HAPPY NEW YEAR!"
+  end
